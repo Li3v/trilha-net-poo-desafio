df167c51500a975ea39356133cd86d5dda33966f
= <=>
2024-04-27 19:45:27 -0300

    desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Nokia");
+Smartphone nokia = new Nokia("666", "Tijolão", "333", 32);
+nokia.Ligar();
+nokia.InstalarAplicativo("outlook");
+
+Console.WriteLine("Iphone");
+Smartphone iphone = new Iphone("0", "X", "999", 32);
+iphone.Ligar();
+iphone.InstalarAplicativo("instagram");
